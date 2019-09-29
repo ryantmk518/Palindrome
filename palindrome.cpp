08bdf6d8ac644e35a31a20d3fd2d80cb35565dca
Ryan Thammakhoune <ryantmk518@gmail.com>
2019-09-28 21:44:34 -0700

    Added comments

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std; 
 
+//Ryan Thammakhoune. Palindrome. This program takes an input and checks if the input is a palindrome.
+
 int main() {
-  char input[81];
-  cin.get(input, 81);
+  char input[81]; //Initialize variables
+  cin.get(input, 81); //Get the input
   cin.get();
-  int length = strlen(input);
+  int length = strlen(input); //Get length of input
   char str[81];
   int num = 0;
   int len = 0;
   bool isPal = true;
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++) { //For loop that puts only allowed characters into the new cstring.
     if ((int) input[i] <= 57 && (int) input[i] >=48) {
       str[num++] = input[i];
       len++;
@@ -29,7 +32,7 @@ int main() {
   }
   str[num] = '\0';
   cout << str << endl;
-  for (int a = 0; a < strlen(str); a++) {
+  for (int a = 0; a < strlen(str); a++) { //Checks to see if the first char is the same as the last etc.
     if (str[a] != str[strlen(str)-1-a]) {
       isPal = false;
     }
